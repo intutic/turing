7f9f4fc734c4d8f3612079e81c3ecdb859f39d05
Ishan Gupta <ishangupta.ds@gmail.com>
2026-08-28 11:26:29 +0530

    perf(triton): optimize Matryoshka sliced gemv with 2D batch grid launch
    Signed-off-by: Ishan Gupta <ishangupta.ds@gmail.com>

--- turing/kernels/triton_matryoshka_spec.py
+++ turing/kernels/triton_matryoshka_spec.py
@@ -17,18 +17,23 @@ except ImportError:
 if HAS_TRITON:
     @triton.jit
     def _matryoshka_sliced_gemv_kernel(
-        x_ptr,           # [1, HiddenDim]
+        x_ptr,           # [BatchSize, HiddenDim]
         w_ptr,           # [VocabSize, HiddenDim]
-        out_ptr,         # [1, VocabSize]
+        out_ptr,         # [BatchSize, VocabSize]
         slice_width: tl.constexpr,
         hidden_dim: tl.constexpr,
         vocab_size: tl.constexpr,
         BLOCK_SIZE_V: tl.constexpr,
         BLOCK_SIZE_K: tl.constexpr,
     ):
-        pid = tl.program_id(axis=0)
-        v_offsets = pid * BLOCK_SIZE_V + tl.arange(0, BLOCK_SIZE_V)
+        pid_v = tl.program_id(axis=0)
+        pid_b = tl.program_id(axis=1)
+
+        v_offsets = pid_v * BLOCK_SIZE_V + tl.arange(0, BLOCK_SIZE_V)
         v_mask = v_offsets < vocab_size
+
+        x_base_ptr = x_ptr + pid_b * hidden_dim
+        out_base_ptr = out_ptr + pid_b * vocab_size
 
         accum = tl.zeros((BLOCK_SIZE_V,), dtype=tl.float32)
 
@@ -36,7 +41,7 @@ if HAS_TRITON:
             k_offsets = k + tl.arange(0, BLOCK_SIZE_K)
             k_mask = k_offsets < slice_width
 
-            x_vals = tl.load(x_ptr + k_offsets, mask=k_mask, other=0.0)
+            x_vals = tl.load(x_base_ptr + k_offsets, mask=k_mask, other=0.0)
 
             # Load tile of weights [BLOCK_SIZE_V, BLOCK_SIZE_K]
             w_ptrs = w_ptr + (v_offsets[:, None] * hidden_dim + k_offsets[None, :])
@@ -44,7 +49,7 @@ if HAS_TRITON:
 
             accum += tl.sum(w_vals * x_vals[None, :], axis=1)
 
-        tl.store(out_ptr + v_offsets, accum, mask=v_mask)
+        tl.store(out_base_ptr + v_offsets, accum, mask=v_mask)
 
 
 def matryoshka_sliced_gemv_triton(
@@ -72,18 +77,18 @@ def matryoshka_sliced_gemv_triton(
     BLOCK_SIZE_V = 64
     BLOCK_SIZE_K = 128
 
-    grid = lambda meta: (triton.cdiv(vocab_size, meta['BLOCK_SIZE_V']), batch_size)
+    grid = (triton.cdiv(vocab_size, BLOCK_SIZE_V), batch_size)
 
-    for b in range(batch_size):
-        _matryoshka_sliced_gemv_kernel[(triton.cdiv(vocab_size, BLOCK_SIZE_V),)](
-            x_flat[b],
-            w,
-            out[b],
-            slice_width=effective_w,
-            hidden_dim=hidden_dim,
-            vocab_size=vocab_size,
-            BLOCK_SIZE_V=BLOCK_SIZE_V,
-            BLOCK_SIZE_K=BLOCK_SIZE_K,
-        )
+    _matryoshka_sliced_gemv_kernel[grid](
+        x_flat,
+        w,
+        out,
+        slice_width=effective_w,
+        hidden_dim=hidden_dim,
+        vocab_size=vocab_size,
+        BLOCK_SIZE_V=BLOCK_SIZE_V,
+        BLOCK_SIZE_K=BLOCK_SIZE_K,
+    )
 
     return out.to(x.dtype).view(*orig_shape[:-1], vocab_size)
+
